cc9a6f1374cc847b8a300ccbb83b8ee4e56e9759
Alexander Kondratyuk <alexander.kondratyuk@gmail.com>
2022-01-05 13:57:42 +0300

    switched to access timing

--- ConsoleApplication8.cpp
+++ ConsoleApplication8.cpp
@@ -11,41 +11,53 @@
 struct mybool
 {
     bool b;
+    constexpr bool operator <(const mybool& b2) const noexcept { return b < b2.b; }
 };
 
 template<typename T>
 void foo(const char* typeName)
 {
-    long long minTime = std::numeric_limits<long long>::max();
+    
     int cycles = 10;
 #ifdef _DEBUG
-    size_t arraySize = (size_t)3;
+    size_t arraySize = (size_t)4;
 #else
     size_t arraySize = (size_t)1e9;
 #endif
 
-    size_t sizeT = sizeof(T);
-    std::cout << typeName << std::endl;
+    std::cout <<  typeName << std::endl;
+    bool dummyBoolResult = false;
+    long long minTime = std::numeric_limits<long long>::max();
+    std::vector<T> v;
+    v.reserve(arraySize);
+    for( size_t j = arraySize; j--; )
+    {
+        v.push_back(T());
+    }
     for( int i = cycles; i--;)
     {
+
         std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-        std::vector<T> v;
-
-        v.reserve(arraySize);
-
-        for( size_t j = arraySize; j--; )
+        
+        auto middle = arraySize / 2u;
+        for( size_t j = middle; j--;)
         {
-            v.push_back(T());
+            const auto& value0 = v[j];
+            const auto& value1 = v[middle + j];
+            dummyBoolResult |= value0 < value1;
         }
-        auto interval = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now() - start).count();
+
+        auto interval = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count();
+      
         if( interval < minTime )
         {
             minTime = interval;
-            std::cout << "" << minTime << "us";
+            std::cout << std::endl << minTime << "ms";
         }
+
         std::cout << '.' << std::flush;
     }
-    std::cout << std::endl;
+    std::cout << dummyBoolResult << "\n\n" << std::flush;
 }
 
 int main()
